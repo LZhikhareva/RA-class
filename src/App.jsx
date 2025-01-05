fd1f83352cce213ddc5d315fd2902cdd0a85fd4e
Liza Zhikhareva <lisalisalisa57@mail.ru>
2025-01-05 10:58:21 +0300

    small changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import React from 'react';
+import ShopItemClass from './ShopItemClass';
 
 const item = {
   brand: 'Tiger of Sweden',
@@ -12,10 +13,6 @@ const item = {
 };
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <div className="container">
@@ -24,33 +21,11 @@ class App extends React.Component {
           <div className="highlight-overlay" />
         </div>
         <div className="window">
-          <ShopItemFunc item={item} />
+          <ShopItemClass item={item} />
         </div>
       </div>
     );
   }
 }
 
-class ShopItemFunc extends React.Component {
-  render() {
-    return (
-      <div className="main-content">
-        <h2>{item.brand}</h2>
-        <h1>{item.title}</h1>
-        <h3>{item.description}</h3>
-        <div className="description">{item.descriptionFull}</div>
-        <div className="highlight-window mobile">
-          <div className="highlight-overlay" />
-        </div>
-        <div className="divider" />
-        <div className="purchase-info">
-          <div className="price">
-            {item.currency}{item.price.toFixed(2)}
-          </div>
-          <button>Добавить в корзину</button>
-        </div>
-      </div>
-    );
-  }
-}
 export default App;
